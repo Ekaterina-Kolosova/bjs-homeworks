1d1089f5bc2d007d0f4e12e86045872188f86cd3
Ekaterina-Kolosova <kolosova411@yandex.ru>
2020-06-23 00:40:23 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	
+    let creditAmount = amount - contribution; 
+    let creditMonth = Math.round((date - new Date()) / (30 * 24 * 1000 * 3600)); 
+    let p = (1 / 12) * (percent / 100);
+    let paymentMonth = creditAmount * (p + p / (Math.pow((1 + p), creditMonth) - 1));
+    let totalAmount = paymentMonth * creditMonth;
+    totalAmount = Number(totalAmount.toFixed(2));
+    console.log(totalAmount)
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting 
+    if (name) {
+    	greeting = `Привет, мир! Меня зовут ${name}`;
+    } else {
+    	greeting = 'Привет, мир! Меня зовут Аноним';
+    }
+
+    console.log(greeting);
+    return greeting;
 }
