fee83ab1456664ca87cddebcf824d8bab3753b54
Ekaterina-Kolosova <61948555+Ekaterina-Kolosova@users.noreply.github.com>
2020-06-26 00:37:53 +0300

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,19 +3,30 @@
 function getResult(a,b,c){
 	let discriminant = Math.pow(b, 2) - (4 * a * c);
 	let x = [];
-    if (discriminant < 0) {
-    	x = [];
-    } else if (discriminant === 0) {
-    	x = [(- 1 * b) / (2 * a)];
+    if (discriminant === 0) {
+    	x = [(-b) / (2 * a)];
     } else if (discriminant > 0) {
-    	x[0] = ((- 1 * b) + Math.sqrt(discriminant)) / (2 * a);
-    	x[1] = ((- 1 * b) - Math.sqrt(discriminant)) / (2 * a);
+    	x[0] = ((-b) + Math.sqrt(discriminant)) / (2 * a);
+    	x[1] = ((-b) - Math.sqrt(discriminant)) / (2 * a);
     }
 
     return x;
 }
 
 function getAverageMark(marks){
+	
+	let averageMark;
+	let marksSum = sumMark(marks);
+
+	if (marks.length === 0) {
+		averageMark = 0;
+	} else if (5 < marks.length) {
+		console.log('Введено более 5 оценок');
+		marksSum = sumMark(marks.splice(5));
+	} 
+	averageMark = marksSum / marks.length;
+	
+
 	function sumMark(){
 		let marksSum = 0;
 		for(let i=0; i < marks.length; i++) {
@@ -24,29 +35,15 @@ function getAverageMark(marks){
 		return marksSum;
 	}
 
-	let averageMark;
-	let marksSum = sumMark(marks);
-
-	if ((0 < marks.length) && (5 >= marks.length)) {
-		averageMark = marksSum / marks.length;
-	} else if (5 < marks.length) {
-		console.log('Введено более 5 оценок');
-		marksSum = sumMark(marks.splice(5))
-		averageMark = marksSum / 5;
-	} else if (marks.length === 0) {
-		averageMark = 0;
-	}
-	
     return averageMark;
 }
 
 
 
 function askDrink(name,dateOfBirthday){
-    let userAge = dateOfBirthday.getFullYear();
-    let year = new Date().getFullYear();
+    let userAge = new Date().getFullYear() - dateOfBirthday.getFullYear();
     let result;
-    if ((year - userAge) > 18) {
+    if (userAge > 18) {
     	result = `Не желаете ли олд-фэшн, ${name}?`;
 	} else {
 		result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
