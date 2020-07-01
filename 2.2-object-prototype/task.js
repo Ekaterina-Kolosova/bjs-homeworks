79cad936b368dd51c5e06bba1c86094d61e12bd2
Ekaterina-Kolosova <61948555+Ekaterina-Kolosova@users.noreply.github.com>
2020-07-02 00:25:19 +0300

    Update task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome;
+
+function isPalindrome() {
+	let lower = this.toLowerCase();
+	if (lower === lower.split('').reverse().join('')) {
+		return true;
+	} else {
+		return false;
+	};
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+	for (let i = 0; i < marks.length; i++) {
+		sum += marks[i];
+	};
+	if (marks.length === 0) {
+		return 0;
+	};
+
+	let average = sum / marks.length;
+	let roundedAverage = Math.round(average);
+	// код для задачи №2 писать здесь
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+	let now = new Date();
+	birthday = new Date(birthday);
+	let diff = now - birthday;
+	console.log(diff);
+	let age = diff / (365.25 * 24 * 1000 * 3600);
+	console.log(age);
+	if (age > 18) {
+		return true;
+	} else {
+		return false;
+	};
 }
