b251c3592c6158ec0169b8459ad64f2ed119b1e2
Ekaterina-Kolosova <61948555+Ekaterina-Kolosova@users.noreply.github.com>
2020-07-03 01:44:06 +0300

    first commit oop

---
+++ 2.3-oop-es6/task.js
@@ -0,0 +1,117 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	};
+
+	
+	set state(newState) {
+		if (newState < 0) {
+			this._state = 0;
+		} else if (newState > 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		};
+	};
+
+	get state() {
+		return this._state;
+	};
+
+	fix() {
+		this.state = this.state * 1.5;
+	};
+};
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount, state) {
+		super(name, releaseDate, pagesCount, state);
+		this.type = 'magazine';
+	}
+};
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount, state) {
+		super(name, releaseDate, pagesCount, state);
+		this.type = 'book';
+		this.author = author;
+	}
+};
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount, state) {
+		super(author, name, releaseDate, pagesCount, state);
+		this.type = 'novel';
+	}
+};
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount, state) {
+		super(author, name, releaseDate, pagesCount, state);
+		this.type = 'fantastic';
+	}
+};
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount, state) {
+		super(author, name, releaseDate, pagesCount, state);
+		this.type = 'detective';
+	}
+};
+
+// Задача № 2
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	};
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		};
+	};
+	findBookBy(type, value) {
+		let foundedBook;
+		for (let i = 0; i < this.books.length; i++) {			
+			if (this.books[i][type] === value) {
+				foundedBook = this.books[i];
+			} else {
+				foundedBook = null;
+			};
+		};
+
+		return foundedBook;
+		
+	};
+	giveBookByName(bookName) {
+		let foundBook;
+		for (let i = 0; i < this.books.length; i++) {
+			if (bookName === this.books[i].name) {
+				foundBook = this.books.splice(i, 1);
+			} else {
+				foundBook = null;
+			};
+		};
+
+		return foundBook;
+	};
+};
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", 'Пикник на обочине')); //null
+console.log(library.findBookBy("releaseDate", 1924)); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
